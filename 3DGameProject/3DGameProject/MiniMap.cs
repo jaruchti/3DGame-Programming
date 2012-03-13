845cceaa451736765f4bc582ebccc27075b7c641
Jason Ruchti <jaruchti@gmail.com>
2012-03-13 16:15:42 -0500

    Added documentation to minimap

--- 3DGameProject/3DGameProject/MiniMap.cs
+++ 3DGameProject/3DGameProject/MiniMap.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * 3D Game Programming Project
+ * Dr. Liu
+ * Zach Bates, Lauren Buss, Corey Darr, Jason Ruchti, Jared Tittle
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,31 +16,54 @@ using Microsoft.Xna.Framework.Input;
 
 namespace _3DGameProject
 {
+    /// <summary>
+    /// Class which implements the logic to display a mini-map with the city layout, 
+    /// the player's position, enemy positions, etc. the screen.
+    /// </summary>
+    /// <remarks>This is a singleton in the game</remarks>
+    /// 
     class MiniMap
     {
         private SpriteBatch spriteBatch;
-        private Texture2D whiteRect;
-        private Texture2D whiteSphere;
+        private Texture2D whiteRect;        // holds a white rectangle texture
+        private Texture2D whiteCircle;      // holds a white circle texture
 
-        const int xOffset = 400;
-        const int yOffset = 40;
-        const int rectWidth = 5;
-        const int rectHeight = 5;
+        /// <summary>xOffset on display to the left side of the mini-map</summary>
+        private const int xOffset = 400;
+        /// <summary>xOffset on display to the top side of the mini-map</summary>
+        private const int yOffset = 40;
+        /// <summary>width of each item in the mini-map</summary>
+        private const int rectWidth = 5;
+        /// <summary>height of each item in the mini-map</summary>
+        private const int rectHeight = 5;
 
+        /// <summary>
+        /// Load the content required for the minimap.
+        /// </summary>
+        /// <param name="device">Graphics card (to initialize spritebatch)</param>
+        /// <param name="content">Content pipeline (for texture)</param>
         public void LoadContent(ref GraphicsDevice device, ContentManager content)
         {
             whiteRect = content.Load<Texture2D>("Textures/whiterect");
 
-            whiteSphere = content.Load<Texture2D>("Textures/whitesphere");
+            whiteCircle = content.Load<Texture2D>("Textures/whitecircle");
             spriteBatch = new SpriteBatch(device);
         }
 
+
+        /// <summary>
+        /// Draw the minimap to the screen
+        /// </summary>
+        /// <param name="player">The position of player is needed for minimap</param>
+        /// <param name="enemies">The position of enemies is needed for minimap</param>
+        /// <param name="map">The position of the fuel barrels and building is needed for minimap</param>
         public void Draw(Player player, Enemies enemies, Map map)
         {
-            Rectangle rect = new Rectangle();
+            Rectangle rect = new Rectangle();   // this rectangle describes the position on screen where an object will be drawn
             rect.Width = rectWidth;
             rect.Height = rectHeight;
 
+            // get items to display
             int[,] floorPlan = map.FloorPlan;
             Fuel[] fuelBarrels = map.FuelBarrels;
             Enemy[] enemyArr = enemies.getEnemiesArray();
@@ -48,6 +77,7 @@ namespace _3DGameProject
                 {
                     if (floorPlan[x, z] != 0)
                     {
+                        // move the rectangle into the correct position of the screen
                         rect.X = xOffset + rectWidth * z;
                         rect.Y = yOffset + rectHeight * x;
 
@@ -61,7 +91,7 @@ namespace _3DGameProject
             {
                 rect.X = xOffset - rectWidth * (int)fuelBarrels[i].Position.Z;
                 rect.Y = yOffset + rectHeight * (int)fuelBarrels[i].Position.X;
-                spriteBatch.Draw(whiteSphere, rect, Color.White);
+                spriteBatch.Draw(whiteCircle, rect, Color.White);
             }
 
             // draw player
@@ -69,6 +99,7 @@ namespace _3DGameProject
             rect.Y = yOffset + rectHeight * (int)player.Position.X;
             spriteBatch.Draw(whiteRect, rect, Color.Green);
 
+            // draw enemies
             for (int i = 0; i < enemyArr.Length; i++)
             {
                 rect.X = xOffset - rectWidth * (int)enemyArr[i].Position.Z;
